10050926b6e86e454ac9c6ea23a000d1338753a2
Clayton <mei22001@byui.edu>
2022-11-12 22:22:59 -0600

    css fixes

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -18,7 +18,7 @@ const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 setElementText("#lastmodified", new Date(document.lastModified).toLocaleString('en-us'))
 setElementText("#currentDate", fulldate);
-setElementText("#date", new Date().toLocaleString('en-us'))
+document.getElementById("date").value =  new Date().toLocaleString('en-us')
 const dayOfWeek = now.getDay();
 if(dayOfWeek == 1 || dayOfWeek == 4){
     document.getElementById("banner").innerText = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."
