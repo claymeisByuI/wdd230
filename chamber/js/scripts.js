1fe5620463fe8c1fa8395b3427c8100fcafc508e
Clayton <mei22001@byui.edu>
2022-11-27 00:10:53 -0600

    updates week 11

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -18,7 +18,10 @@ const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 setElementText("#lastmodified", new Date(document.lastModified).toLocaleString('en-us'))
 setElementText("#currentDate", fulldate);
-document.getElementById("date").value =  new Date().toLocaleString('en-us')
+const dateElement = document.getElementById("date");
+if(dateElement){
+    dateElement.value =  new Date().toLocaleString('en-us');
+}
 const dayOfWeek = now.getDay();
 if(dayOfWeek == 1 || dayOfWeek == 4){
     document.getElementById("banner").innerText = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."
@@ -72,3 +75,41 @@ window.localStorage.setItem("LastVisitedOn", Date.now());
 // show todays date.
 LastVisitedElement.textContent =  LastVisitedOn.toLocaleString('en-us');
 DaysSincePageVisitElement.textContent = DiffInDays.toFixed(0);
+
+
+const requestURL = 'json/data.json';
+fetch(requestURL)
+.then(function (response) {
+    return response.json();
+})
+.then(function (jsonObject) {
+
+    return jsonObject.filter(function (b){
+        if(b["membership-level"] == "gold" 
+        || b["membership-level"] == "silver"){
+            return true;
+        }
+        return false;
+    })
+})
+.then(function (jsonObject) {
+    const spotlight1 = document.querySelector('.grid--spotlight-1'); 
+    const spotlight2 = document.querySelector('.grid--spotlight-2'); 
+    const businesss = jsonObject;
+    var business1 = businesss[~~(Math.random() * businesss.length)];
+    var business2 = businesss[~~(Math.random() * businesss.length)];
+    console.dir(business1)
+    console.dir(business2)
+    spotlight1.setHTML(BuildCard(business1));    
+    spotlight2.setHTML(BuildCard(business2));    
+});
+
+function BuildCard(business) {
+    return `
+            <h3>${business.name}</h3>
+            <div class="image"><img src="${business.logo}" alt="${business.name} logo"></div>
+            <hr>
+            <p>✉️<a href="email:${business.email}">${business.email}</a></p>
+            <p>📞<a href="tel:${business.Phone}">${business.Phone}</a> <a href="${business.url}">Website</a></p>
+            `;
+}
